4291090719d6bdda87c680068936f42c23c699c6
Salavat <ibooster197@gmail.com>
2022-05-18 19:59:31 +0600

    grade

--- students/views.py
+++ students/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-from .models import Student
+from django.shortcuts import render, get_object_or_404
+from .models import Student 
 
 def student_list(request):
 	students = Student.objects.all()
+	search_query = request.GET.get('q')
+	grade_query = request.GET.get('grade')
+	if search_query:
+		students = students.filter(name__contains=search_query)
+	if grade_query:
+		students = students.filter(grade=grade_query)
 	context ={
-		'students':students
+		'students':students,
+		'search_query':search_query,
+		'grade_query':grade_query
 	}
 	return render(request, 'students/student_list.html', context)
 
